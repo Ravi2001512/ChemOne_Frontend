43488f76507842b5ddcc2a4c21d8438836dd2842
Chanu Haintha <116580398+ChanuHasintha@users.noreply.github.com>
2026-05-24 10:42:06 +0530

    remove to vesak lamps

--- src/components/VesakDecorations.jsx
+++ src/components/VesakDecorations.jsx
@@ -214,18 +214,6 @@ const VesakDecorations = ({ children }) => {
           </div>
         </div>
 
-        {/* Traditional Clay Lamps (Vesak Pahan) at the bottom */}
-        <div className="vesak-lamps">
-          {Array.from({ length: 6 }).map((_, idx) => (
-            <div key={idx} className="paha-lamp">
-              <div className="lamp-flame">
-                <div className="flame-inner" />
-              </div>
-              <div className="lamp-wick" />
-              <div className="lamp-clay" />
-            </div>
-          ))}
-        </div>
       </div>
 
       {/* Content Layer */}
